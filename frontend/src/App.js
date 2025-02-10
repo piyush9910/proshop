04c05712e585ae992a59189baaab2efebb69493c
piyush9910 <suripiyush4@gmail.com>
2025-02-10 08:42:37 +0530

    custom css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
+import { Container } from 'react-bootstrap';
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <div>Welcome to ProShop</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
