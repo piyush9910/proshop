ac40fc1f274fc0747cad09150980d015ab9f8354
piyush9910 <suripiyush4@gmail.com>
2025-02-11 09:07:53 +0530

    Rating and Product Detail added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap';
 import React from 'react';
 import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 
 const App = () => {
@@ -10,7 +10,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
