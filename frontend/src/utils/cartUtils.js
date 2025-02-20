f3aca493f58df1f5f5e46cbec713225c5bb15f32
piyush9910 <suripiyush4@gmail.com>
2025-02-20 08:16:34 +0530

    Add CartScreen component and implement cart functionality with item removal

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -1,21 +1,27 @@
-export const addDecimal = (num) => {
+export const addDecimals = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2);
 };
 
 export const updateCart = (state) => {
-  // Calculate items price
-  state.itemsPrice = addDecimal(
+  // Calculate the items price
+  state.itemsPrice = addDecimals(
     state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   );
-  // Calculate shipping price (If order is over 1000 then free, else 50)
-  state.shippingPrice = state.itemsPrice > 1000 ? 0 : 50;
-  // Calculate tax price (5% GST)
-  state.taxPrice = addDecimal(Number((0.05 * state.itemsPrice).toFixed(2)));
-  // Calculate total price
-  state.totalPrice =
+
+  // Calculate the shipping price
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+  // Calculate the tax price
+  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+
+  // Calculate the total price
+  state.totalPrice = (
     Number(state.itemsPrice) +
     Number(state.shippingPrice) +
-    Number(state.taxPrice).toFixed(2);
+    Number(state.taxPrice)
+  ).toFixed(2);
+
+  // Save the cart to localStorage
   localStorage.setItem('cart', JSON.stringify(state));
 
   return state;
